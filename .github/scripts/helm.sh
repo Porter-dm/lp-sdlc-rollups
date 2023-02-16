829330516ad2da8846554ee60b1f2e26b422d530
Andrew Hyson <andi.hyson@dotmatics.com>
2023-02-16 13:22:39 +0000

    Improved package and publish process

--- .github/scripts/helm.sh
+++ .github/scripts/helm.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 FULL_VERSION=$1
+REPLACE_STRING="__VERSION__"
+ZIPFILE="zebra-sdlc-rollup-$FULL_VERSION.tgz"
+CHART_DIR="./Chart"
 
-echo "This is the package script"
-echo "Some Message = $JFROG_TOKEN
-echo "FULL_VERSION = $FULL_VERSION
+echo "Packaging Helm Chart - $FULL_VERSION AS $ZIPFILE"
+
+# Replace version number
+sed -i "s/$REPLACE_STRING/$FULL_VERSION/" $CHART_DIR/Chart.yaml
+
+# update repo index
+helm repo update
+
+# Update dependencies (e.g. download them)
+helm dependency update $CHART_DIR
+
+# Package file
+helm package $CHART_DIR
